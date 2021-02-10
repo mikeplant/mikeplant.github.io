078a51bf8419e25c9ff7e94e808cf1d18ff463be
mikeplant <michaelplant@gmail.com>
2021-02-10 22:52:49 +0000

    Add add book page forms

--- js/ViewHandler.js
+++ js/ViewHandler.js
@@ -43,7 +43,35 @@ class ViewHandler {
                 })
             },
             addItem: () => {
-                this.mainHeading.textContent = 'Add Items';
+                this.mainHeading.textContent = 'Add Items'
+                viewHandler.itemsDiv.innerHTML = `<div id="add-items-div">
+                <div class="add-item-form-div">
+                  <form>
+                    <h3>Add a book manually:</h3>
+                    <label for="add-book-title">Title: </label>
+                      <input type="text" id="add-book-title" required>
+                    <label for="add-book-author">Author: </label>
+                      <input type="text" id="add-book-author" required>
+                    <label for="add-book-series">Series: </label>
+                      <input type="text" id="add-book-series">
+                    <label for="add-book-genre">Genre: </label>
+                      <input type="text" id="add-book-genre">
+                    <label for="add-book-pages">Pages: </label>
+                      <input type="text" id="add-book-pages">
+                    <label for="add-book-isbn">ISBN: </label>
+                      <input type="text" id="add-book-isbn" required>
+                    <label for="add-book-stock-quantity">Add Stock: </label>
+                      <input type="text" id="add-book-stock-quantity" required>
+                    <button formaction="submit" class="selector-btn">Add</button>
+                  </form>
+                </div>
+      
+                <div class="add-book-search-div">
+                  <h2>Search for a book:</h2>
+                  <input type="text">
+                  <p>Find a book to add to the library by any of it's properties.<br>
+                     For best results use the book's title, author, or ISBN number.</p>
+                </div>`;
             },
             editItem: () => {
                 this.mainHeading.textContent = 'Edit Items';
